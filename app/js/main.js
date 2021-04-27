342c6c5f7f3ebdc0a78efb08032fa013a7c5e2e9
kirill.pr9n1k <kirill.pr9n1k@yandex.ru>
2021-04-27 19:42:13 +0300

    Добавил в меню,которое находится в сайдбаре, под-меню

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
 $(function () {
-
+   $('.menu__link').on('click',function(){
+      var btn = $(this);
+      var btnParent = btn.closest('.menu__item');
+      btnParent.toggleClass('open');
+   })
 })
