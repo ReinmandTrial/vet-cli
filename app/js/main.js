194740d831071ef7521e4da976a2bdc20bc5667e
kirill.pr9n1k <kirill.pr9n1k@yandex.ru>
2021-04-28 15:34:57 +0300

    Слайдеры, шаблонные блоки на первой странице

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,37 @@ $(function () {
       var btnParent = btn.closest('.menu__item');
       btnParent.toggleClass('open');
    })
+   // $('.top__slider').slick({
+   //    dots: true,
+   //    arrows: false,
+   //    fade: true,
+   //    autoplay: true
+   // });
+   
+   // Слайдер 
+   $('.slick').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true
+    });
+    $('.slick--second').slick({
+       infinite: true,
+       slidesToShow: 4,
+       slidesToScroll: 1,
+       arrows: true
+    })
+    $('.slick-next').html('<span class="icon icon-angle-right"></span>');
+    $('.slick-prev').html('<span class="icon icon-angle-left"></span>');
+   //  Слайдер конец 
+   //switch block
+    $('.switch__item').on('click',function(){
+       var btn = $(this)
+       var btnItem = btn.closest('.switch__list').find('.switch__item');
+      $(btnItem).each(function(){
+         $(this).removeClass('active');
+      })
+      btn.addClass('active');
+    })
+   //switch block end
 })
